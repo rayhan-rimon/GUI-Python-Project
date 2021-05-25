e2481e6e2b6b771e518bd9193e8de6271a35ebcd
Md. Abu Rayhan Rimon <69951387+rayhan-rimon@users.noreply.github.com>
2021-05-25 04:49:37 -0700

    GUI Calculator

--- calculator GUI/GUI_calculator.py
+++ calculator GUI/GUI_calculator.py
@@ -8,6 +8,7 @@ window.resizable(False, False) # Window width & height resize off
 window.config(bg='#A9A9A9') # Calculator BG color
 window.iconbitmap('icon.ico') # Change icon
 
+# Main program start...
 expression = ''
 def press(num):
     global expression
@@ -39,7 +40,11 @@ button_frame.pack()
 
 equation = StringVar()
 equation.set('0')
+# Main Program end
+
+# Calculator button Style Start...
 expression_field = Entry(button_frame, textvariable=equation, justify='right', font=('arial', 20, 'bold'))
+
 
 button1 = Button(button_frame, text='1',font=('time new roman',12), relief='ridge', bd=1, bg='#A9A9A9', width=8, height=3, command=lambda:press(1))
 button2 = Button(button_frame, text='2',font=('time new roman',12), relief='ridge', bd=1, bg='#A9A9A9', width=8, height=3, command=lambda:press(2))
@@ -62,30 +67,38 @@ clear = Button(button_frame, text='c',font=('time new roman',12), relief='ridge'
 division = Button(button_frame, text='/',font=('time new roman',12), relief='ridge', bd=1, bg='#A9A9A9', width=8, height=3, command=lambda:press('/'))
 
 equal = Button(button_frame, text='=',font=('time new roman',12), relief='ridge', bd=1, bg='#A9A9A9', width=35, height=3, command=equalpress)
+# Calculator Button style end..
 
-expression_field.grid(row=0, column=0, columnspan=4, ipadx=5, ipady=25, pady=15)
+# 0 row mean 1st line  Display Position on this screen
+expression_field.grid(row=0, column=0, columnspan=4, ipadx=5, ipady=25, pady=15) #display position
 
-button1.grid(row=1, column=0)
-button2.grid(row=1, column=1)
-button3.grid(row=1, column=2)
-addition.grid(row=1, column=3)
+# 1st row mean 2nd line Button Position on this screen
+button1.grid(row=1, column=0) 
+button2.grid(row=1, column=1)  
+button3.grid(row=1, column=2)  
+addition.grid(row=1, column=3)  
 
-button4.grid(row=2, column=0)
-button5.grid(row=2, column=1)
-button6.grid(row=2, column=2)
-subtract.grid(row=2, column=3)
+# 2nd row mean 3rd line  Button Position on this screen
+button4.grid(row=2, column=0)  
+button5.grid(row=2, column=1)  
+button6.grid(row=2, column=2)  
+subtract.grid(row=2, column=3)  
 
-button7.grid(row=3, column=0)
-button8.grid(row=3, column=1)
-button9.grid(row=3, column=2)
-multiply.grid(row=3, column=3)
+# 3rd row mean 4th line  Button Position on this screen
+button7.grid(row=3, column=0)  
+button8.grid(row=3, column=1)  
+button9.grid(row=3, column=2) 
+multiply.grid(row=3, column=3) 
 
-button0.grid(row=4, column=0)
-decimal.grid(row=4, column=1)
-clear.grid(row=4, column=2)
-division.grid(row=4, column=3)
+# 4th row mean 5th line  Button Position on this screen
+button0.grid(row=4, column=0) 
+decimal.grid(row=4, column=1) 
+clear.grid(row=4, column=2) 
+division.grid(row=4, column=3) 
 
+# 5th row mean 6th line  Button Position on this screen
 equal.grid(row=5, column=0, columnspan=4)
+
 
 window.mainloop()
 
